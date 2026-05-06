24b7cb666c7a1bbf9754e79c465a4839c414021c
dsad233 <ksl1421@naver.com>
2026-05-06 21:14:15 +0900

    fix: cookie-parser 추가에 따른 main.ts 수정

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
-import express, { Express, Request, Response, NextFunction } from "express";
-import AuthRouter from "./auth/auth.router";
-import { ErrorMiddleware } from "./common/middlewares/errorMiddleware";
-import { HttpError } from "http-errors";
-import { prisma } from "./common/configs/prisma-client";
-import { redisCondition } from "./redis/redis.config";
+import express, { Express, Request, Response, NextFunction } from 'express';
+import AuthRouter from './auth/auth.router';
+import UsersRouter from './users/users.router';
+
+import ErrorMiddleware from './common/middlewares/errorMiddleware';
+
+import { HttpError } from 'http-errors';
+import { prisma } from './common/configs/prisma-client';
+import { redisCondition } from './redis/redis.config';
+import cookieParser from 'cookie-parser';
 
 const app: Express = express();
 const port: number = 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 
 // Redis 상태 체크
@@ -16,18 +21,18 @@ redisCondition();
 
 app.use("/auth", AuthRouter);
 
-app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
-  ErrorMiddleware(error, req, res, next);
-});
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) =>
+  ErrorMiddleware(error, req, res, next),
+);
 
 app.listen(port, () => {
   prisma.$connect();
-  console.log(port, "실행중...");
+  console.log(port, '실행중...');
 });
 
 // 종료 이벤트
-process.on("exit", () => {
-  console.log("애플리케이션 종료중...");
+process.on('exit', () => {
+  console.log('애플리케이션 종료중...');
 
   setTimeout(() => {
     prisma.$disconnect();
