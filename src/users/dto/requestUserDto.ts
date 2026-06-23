a8623b51385ac143b82ed52ac13c6c6bfc5cf09c
dsad233 <ksl1421@naver.com>
2026-06-23 20:17:29 +0900

    refactor: requestuserdto 수정

--- src/users/dto/requestUserDto.ts
+++ src/users/dto/requestUserDto.ts
@@ -5,18 +5,18 @@ export type IRequestUserDto = {
   id: string;
 };
 
-export async function RequestUserDto(
-  reqUser: IRequestUserDto,
-): Promise<IRequestUserDto> {
-  if (!reqUser?.id) {
+export async function RequestUserDto({
+  id,
+}: IRequestUserDto): Promise<IRequestUserDto> {
+  if (!id) {
     throw new NotFound('파라미터가 존재하지 않습니다. 다시 요청해 주세요.');
   }
 
-  if (!reqUser.id.trim().match(regEx.uuidv4)) {
-    throw new BadRequest('유효하지 않은 ID 형식입니다.');
+  if (!id.trim().match(regEx.uuidv4)) {
+    throw new BadRequest('유효하지 않은 유저 ID 형식입니다.');
   }
 
   return {
-    id: reqUser.id.trim(),
+    id: id.trim(),
   };
 }
